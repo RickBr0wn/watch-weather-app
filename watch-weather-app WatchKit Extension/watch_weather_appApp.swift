80531777b544c72429d2b058fea821046f295ac1
Rick Brown <ricky.brown.00@gmail.com>
2021-03-06 16:43:44 +0000

    Initial Commit

---
+++ watch-weather-app WatchKit Extension/watch_weather_appApp.swift
@@ -0,0 +1,19 @@
+//
+//  watch_weather_appApp.swift
+//  watch-weather-app WatchKit Extension
+//
+//  Created by Rick Brown on 06/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct watch_weather_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
