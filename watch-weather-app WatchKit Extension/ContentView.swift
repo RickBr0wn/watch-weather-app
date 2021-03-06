afd7f519c1de38212c5b2d85ddfe15740bc7cd68
Rick Brown <ricky.brown.00@gmail.com>
2021-03-06 16:55:50 +0000

    adds location privacy settings

--- watch-weather-app WatchKit Extension/ContentView.swift
+++ watch-weather-app WatchKit Extension/ContentView.swift
@@ -7,15 +7,24 @@
 
 import SwiftUI
 
+/**
+ First task is to set the privacy options.
+ Add these 2 properties to the info.plist file:
+  Privacy - Location Always and When In Use Usage Description
+  Privacy - Location Always Usage Description
+ A descripter can be used to explain to the user why.
+  eg: Your location is required to give you accurate weather data for your area.
+*/
+
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
-            .padding()
-    }
+  var body: some View {
+    Text("Hello, World!")
+      .padding()
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
